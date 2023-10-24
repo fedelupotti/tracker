187bd7561cca5c15ef7b093b7e8ed07b894f8bbb
Federico Lupotti <flupotti@applica.dev>
2023-10-24 12:25:53 -0300

    Initial Commit

---
+++ Tracker/TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerApp.swift
+//  Tracker
+//
+//  Created by Federico Lupotti on 24/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
